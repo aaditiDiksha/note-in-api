93669960fe7f2cee2b4ca8d253d4870811121f00
aaditidiksha <aditi.d1231@gmail.com>
2021-08-07 19:30:56 +0530

    debugging

--- controllers/todoItems.js
+++ controllers/todoItems.js
@@ -2,61 +2,61 @@ const handleSaveTodo = (req, res, data) => {
   const { userId, todoId } = req.params;
   const { listItems } = req.body;
   var inc = 0;
-console.log(listItems)
+  console.log(listItems);
   data("todoitems")
     .del()
     .where("todoid", "=", todoId)
     .then((deletedItems) => {
-     if(listItems === [])
-    {
-      console.log('mmm inside if')
-      data
-        .select("*")
-        .from("todoitems")
-        .where("userid", "=", userId)
-        .then((allItems) => {
-          console.log(allItems)
-          res.json(allItems);
-        })
-        .catch((err) => {
-          console.log("error in selecting all items");
-          res.status(400).json("unable to save");
-        });
-    }
-else{
-
-  listItems.map((item) => {
-      console.log("mmm inside else");
-
-    data
-    .insert({
-      todoid: todoId,
-            content: item.content,
-            userid: userId,
-            ischecked: item.ischecked,
-          })
-          .into("todoitems")
-          .returning("*")
-          .then((currentItem) => {
-            inc++;
-            if (inc === listItems.length) {
-              data
-              .select("*")
-              .from("todoitems")
-              .where("userid", "=", userId)
-              .then((allItems) => {
-                res.json(allItems);
-              })
-              .catch((err) => {
-                console.log("error in selecting all items");
-                res.status(400).json("unable to save");
-              });
-            }
+      console.log('am i even getting here')
+      if (listItems === []) {
+        console.log("mmm inside if");
+        data
+          .select("*")
+          .from("todoitems")
+          .where("userid", "=", userId)
+          .then((allItems) => {
+            console.log(allItems);
+            res.json(allItems);
           })
           .catch((err) => {
-            // error = "error in adding new todo items "
-            console.log(err);
+            console.log("error in selecting all items");
+            res.status(400).json("unable to save");
           });
+      } else {
+        listItems.map((item) => {
+          console.log("mmm inside else");
+
+          data
+            .insert({
+              todoid: todoId,
+              content: item.content,
+              userid: userId,
+              ischecked: item.ischecked,
+            })
+            .into("todoitems")
+            .returning("*")
+            .then((currentItem) => {
+              inc++;
+              if (inc === listItems.length) {
+                data
+                  .select("*")
+                  .from("todoitems")
+                  .where("userid", "=", userId)
+                  .then((allItems) => {
+                    res.json(allItems);
+                    console.log("all items");
+                  })
+                  .catch((err) => {
+                    console.log("error in selecting all items");
+                    res.status(400).json("unable to save");
+                  });
+              }
+            })
+            .catch((err) => {
+              // error = "error in adding new todo items "
+              console.log("error here");
+              console.log(err);
+            });
         }); //map ends
       }
     })
